eb0753aff237ea83a72f2dbf0267ddc63ba9a899
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-24 13:44:45 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,22 @@
+def instantiate_new_array
+my_new_array = [ ]
+end
+
+def array_with_two_elements
+my_two_array = ["primeiro", "segundo"]
+end
+
+def first_element
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+my_first_element = taylor_swift.shift
+end
+
+def third_element
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+taylor_swift(3)
+end
+
+def last_element
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  taylor_swift.last 
+end
